2b4bd0ad34b4090b3b20aecfa081cb79926629a8
Sam <samfelt@samfelt-mbp.local>
2020-09-30 14:03:34 -0700

    Fix brew cask outdated error; General organization

--- morning_update.sh
+++ morning_update.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-#------------[ Colors ]--------------------------------------------------------
-Red='\033[0;31m'
-Green='\033[0;32m'
-Blue='\033[0;34m'
-White='\033[0;37m'
-BRed='\033[1;31m'
-BGreen='\033[1;32m'
-BBlue='\033[1;34m'
-BWhite='\033[1;37m'
-NC='\033[m'
+#------------[ Sources ]-------------------------------------------------------
+SOURCES=( colors.sh \
+          banners.sh )
+
+for f in ${SOURCES[@]}; do
+    [ -f $f ] && . $f
+done
 
 #------------[ True/False ]----------------------------------------------------
 
@@ -30,80 +27,6 @@ function line_break()
     else
         printf "%$(tput cols)s\\n" | tr ' ' -
     fi
-}
-
-#------------[ Banners ]-------------------------------------------------------
-function apt_banner()
-{
-    line_break '='
-    echo "        ###    ########  ########          ######   ######## ######## "
-    echo "       ## ##   ##     ##    ##            ##    ##  ##          ##    "
-    echo "      ##   ##  ##     ##    ##            ##        ##          ## "
-    echo "     ##     ## ########     ##    ####### ##   #### ######      ## "
-    echo "     ######### ##           ##            ##    ##  ##          ## "
-    echo "     ##     ## ##           ##            ##    ##  ##          ## "
-    echo "     ##     ## ##           ##             ######   ########    ## "
-    line_break '='
-}
-
-function brew_banner()
-{
-    line_break '='
-    echo "              ____                    "
-    echo "             |  _ \\                   "
-    echo "             | |_) |_ __ _____      __"
-    echo "             |  _ <| '__/ _ \\ \\ /\\ / /"
-    echo "             | |_) | | |  __/\\ V  V / "
-    echo "             |____/|_|  \\___| \\_/\\_/  "
-    line_break '='
-}
-
-function macos_banner()
-{
-    line_break '='
-    echo "                                 ___  __    "
-    echo "              /\\/\\   __ _  ___  /___\\/ _\\   "
-    echo "             /    \\ / _\` |/ __|//  //\\ \\    "
-    echo "            / /\\/\\ \\ (_| | (__/ \\_// _\\ \\   "
-    echo "            \\/    \\/\\__,_|\\___\\___/  \\__/   "
-    line_break '='
-}
-
-function pip2_banner()
-{
-    line_break '='
-    echo "                    _      ____"
-    echo "              _ __ (_)_ __|___ \\"
-    echo "             | '_ \\| | '_ \\ __) |"
-    echo "             | |_) | | |_) / __/ "
-    echo "             | .__/|_| .__/_____|"
-    echo "             |_|     |_|         "
-    line_break '='
-}
-
-function pip3_banner()
-{
-    line_break '='
-    echo "                        _      _____"
-    echo "                 ____  (_)___ |__  /"
-    echo "                / __ \\/ / __ \\ /_ < "
-    echo "               / /_/ / / /_/ /__/ / "
-    echo "              / .___/_/ .___/____/  "
-    echo "             /_/     /_/            "
-    line_break '='
-}
-
-function vim_plugins_banner()
-{
-    line_break '='
-    echo "              _/      _/  _/                      _/_/_/    _/ _/                      "
-    echo "             _/      _/      _/_/_/  _/_/        _/    _/  _/  _/    _/    _/_/_/ _/_/_/      _/_/_/   "
-    echo "            _/      _/  _/  _/    _/    _/      _/_/_/    _/  _/    _/  _/    _/  _/ _/    _/  _/_/        "
-    echo "             _/  _/    _/  _/    _/    _/      _/        _/  _/    _/  _/    _/  _/ _/    _/      _/_/     "
-    echo "              _/      _/  _/    _/    _/      _/        _/    _/_/_/    _/_/_/  _/ _/    _/  _/_/_/        "
-    echo "                                                                           _/ "
-    echo "                                                                       _/_/ "
-    line_break '='
 }
 
 #------------[ Usage ]---------------------------------------------------------
@@ -195,7 +118,7 @@ function update_brew()
             run_command "brew upgrade $package" "Upgrading $package"
         done
     fi
-    run_command 'brew cask outdated' 'Determining outdated cask packages:'
+    run_command 'brew outdated --cask' 'Determining outdated cask packages:'
 #    command_output="$command_output" | tr "\n" " "
     if [ -z "$command_output" ]; then
         echo -e "    ${Blue}[i]${NC} No outdated cask packages"
